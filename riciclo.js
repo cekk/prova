5edb4d7330bfe51f3fe6faab3b8d81cf5a5ba8dd
Andrea Cecchi <andrea.cecchi85@gmail.com>
2014-01-20 20:27:00 +0100

    fixed js

--- riciclo.js
+++ riciclo.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var current_item;
+    var navigation_keys = [37, 38, 39, 40];
     function estrai(callback) {
         $.getJSON("dizionario.json", function(json) {
                 var keys = Object.keys(json);
@@ -14,25 +15,27 @@ $(document).ready(function() {
         current_item = callback;
     });
     $('html').keydown(function(e){
-        if (e.which == current_item.tasto) {
-            $(".jumbotron").before('<div class="alert alert-success">Bravo!</div>');
-            $('.jumbotron').addClass("alert alert-success");
-            setTimeout(function() {
-                $('.jumbotron').removeClass("alert alert-success");
-                $('.alert').remove();
-                estrai(function(callback) {
-                    current_item = callback;
-                });
-            },1000);
+        if ($.inArray(e.which, navigation_keys) === -1) {
+            if (e.which == current_item.tasto) {
+                $(".jumbotron").before('<div class="alert alert-success">Bravo!</div>');
+                $('.jumbotron').addClass("alert alert-success");
+                setTimeout(function() {
+                    $('.jumbotron').removeClass("alert alert-success");
+                    $('.alert').remove();
+                    estrai(function(callback) {
+                        current_item = callback;
+                    });
+                },1000);
 
-        }
-        else {
-            $(".jumbotron").before('<div class="alert alert-danger">Sbagliato!</div>');
-            $('.jumbotron').addClass("alert alert-danger");
-            setTimeout(function() {
-                $('.jumbotron').removeClass("alert alert-danger");
-                $('.alert').remove();
-            },1000);
+            }
+            else {
+                $(".jumbotron").before('<div class="alert alert-danger">Sbagliato!</div>');
+                $('.jumbotron').addClass("alert alert-danger");
+                setTimeout(function() {
+                    $('.jumbotron').removeClass("alert alert-danger");
+                    $('.alert').remove();
+                },1000);
+            }
         }
     });
 });
